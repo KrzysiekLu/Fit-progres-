9712708c3f91c60415dd5722df54d1ad7ec62248
Krzysiek <krzysztof.luuczak@gmail.com>
2024-04-30 22:38:07 +0200

    test

---
+++ main.js
@@ -0,0 +1,26 @@
+// Pobierz dane kroków z Google Fit API
+async function fetchFitData() {
+    const accessToken = 'YOUR_ACCESS_TOKEN'; // Tutaj należy wstawić token dostępowy
+  
+    try {
+      const response = await fetch('https://www.googleapis.com/fitness/v1/users/me/dataSources/derived:com.google.step_count.delta:com.google.android.gms:estimated_steps/datasets/0-1000000000000', {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Dane kroków:', data);
+      } else {
+        console.error('Wystąpił błąd podczas pobierania danych:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Wystąpił błąd podczas pobierania danych:', error);
+    }
+  }
+  
+  fetchFitData();
+  
